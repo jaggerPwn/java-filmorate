6e0cd3b5019a7fdd92e66bfaf26fc8c10f41ffd0
Jagger <mozgmbi@yandex.ru>
2023-04-12 10:34:50 +0300

    added classes added controllers added exceptions added logger test passed in postman

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/films")
+public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int id = 0;
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28", DateTimeFormatter.ISO_LOCAL_DATE))) {
+            throw new ValidationException("дата релиза должна быть не раньше 28 декабря 1895 года");
+        }
+        film.setId(++id);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film put(@RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new ValidationException("No such film id: " + film.getId());
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
+}
