ee90d38481a2d875ebb9bc5d7fe9121fafffe483
Mozgmbi <mozgmbi@yandex.ru>
2023-04-14 13:46:42 +0300

    Добавил тестов и устранил замечания

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ResponseStatus(value = BAD_REQUEST)
